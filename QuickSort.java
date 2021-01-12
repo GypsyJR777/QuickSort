1d5fdd6497510bbe9f121e38af15a5cd9d3d4f30
GypsyJR <42184326+GypsyJR777@users.noreply.github.com>
2021-01-12 18:51:35 +0300

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,32 +1,26 @@
-// По возрастанию    
+// РџРѕ РІРѕР·СЂР°СЃС‚Р°РЅРёСЋ
     public static void quickSort(int[] source, int leftBorder, int rightBorder) {
         int leftMarker = leftBorder;
         int rightMarker = rightBorder;
         int pivot = source[(leftMarker + rightMarker) / 2];
         do {
-            // Двигаем левый маркер слева направо пока элемент меньше, чем pivot
             while (source[leftMarker] < pivot) {
                 leftMarker++;
             }
-            // Двигаем правый маркер, пока элемент больше, чем pivot
             while (source[rightMarker] > pivot) {
                 rightMarker--;
             }
-            // Проверим, не нужно обменять местами элементы, на которые указывают маркеры
             if (leftMarker <= rightMarker) {
-                // Левый маркер будет меньше правого только если мы должны выполнить swap
                 if (leftMarker < rightMarker) {
                     int tmp = source[leftMarker];
                     source[leftMarker] = source[rightMarker];
                     source[rightMarker] = tmp;
                 }
-                // Сдвигаем маркеры, чтобы получить новые границы
                 leftMarker++;
                 rightMarker--;
             }
         } while (leftMarker <= rightMarker);
 
-        // Выполняем рекурсивно для частей
         if (leftMarker < rightBorder) {
             quickSort(source, leftMarker, rightBorder);
         }
@@ -35,7 +29,7 @@
         }
     }
 
-// По убыванию
+// РџРѕ СѓР±С‹РІР°РЅРёСЋ
     public static void quickSort(int[] source, int leftBorder, int rightBorder){
 
         int leftMarker = leftBorder;
